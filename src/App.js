64911642c82d91ddb3bb4c58dda5328a114e104f
Eirik Vilpponen <eirik.vilpponen@gmail.com>
2022-11-16 20:43:48 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDgBvjjrvV7TJywOxlRX9YvZovA_DyNk5c",
+    authDomain: "eirik-chat.firebaseapp.com",
+    projectId: "eirik-chat",
+    storageBucket: "eirik-chat.appspot.com",
+    messagingSenderId: "165015298457",
+    appId: "1:165015298457:web:4430bb5b6d2a4692da3308"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
 function App() {
   return (
     <div className="App">
